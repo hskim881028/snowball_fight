ca432804bab633e6f44a11065dd4010b4ea84d59
hskim <andykim881028@gmail.com>
2020-12-06 23:23:09 +0900

    Add NetworkGeneral

---
+++ Assets/Script/Common/Util/NetworkGeneral.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SF.Common.Packet;
+
+namespace SF.Common.Util {
+    public static class NetworkGeneral {
+        public static readonly int PacketTypesCount = Enum.GetValues(typeof(PacketType)).Length;
+
+        public const int MaxGameSequence = 1024;
+        private const int HalfMaxGameSequence = 1024 / 2;
+
+        public static int SeqDiff(int a, int b) {
+            return Diff(a, b, HalfMaxGameSequence);
+        }
+
+        private static int Diff(int a, int b, int halfMax) {
+            return (a - b + halfMax * 3) % (halfMax * 2) - halfMax;
+        } 
+    }
+}
