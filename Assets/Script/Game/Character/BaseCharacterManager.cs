49221aab5d979803cc9ad27178052849e4652751
hskim <andykim881028@gmail.com>
2020-12-09 22:56:49 +0900

    Add BaseCharacterManager

---
+++ Assets/Script/Game/Character/BaseCharacterManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SF.Character {
+    public abstract class BaseCharacterManager : IEnumerable<Character> {
+        public abstract IEnumerator<Character> GetEnumerator();
+        
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+
+        public abstract void UpdateLogic();
+    }
+}
