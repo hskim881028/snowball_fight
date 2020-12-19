90b63defab4aa8f3e42aee990fe3ba2147ebaca0
hskim <andykim881028@gmail.com>
2020-12-20 00:32:51 +0900

    Fix gameconfig

--- Assets/Script/Common/Util/Gameconfig.cs
+++ Assets/Script/Common/Util/Gameconfig.cs
@@ -1,5 +1,9 @@
-﻿namespace SF.Common.Util {
-    public class Gameconfig {
-        
+﻿using UnityEngine;
+
+namespace SF.Common.Util {
+    public static class Gameconfig {
+        public static Vector2 HomeTeamStartingPoint = new Vector2(-3, 0);
+        public static Vector2 AwayTeamStartingPoint = new Vector2(3, 0);
+        public static Vector2 GroundSize = new Vector2(6, 6);
     }
 }
